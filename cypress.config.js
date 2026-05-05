358975c43aa7116b7daba0f549d1c4b1b1bbf65f
RobaSamoudi <samoudiruba@gmail.com>
2026-05-05 09:59:18 +0300

    UPDATE

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
+  
+  allowCypressEnv: true,
 
   e2e: {
     baseUrl:"https://kewi.ps/",
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    defaultCommandTimeout: 6000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
