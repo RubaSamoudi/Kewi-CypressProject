efec79ddafb8a977d66cdd5fe8c2eeb55df37926
RobaSamoudi <samoudiruba@gmail.com>
2026-05-05 21:02:38 +0300

    update

---
+++ cypress/e2e/UiComponent.cy.js
@@ -0,0 +1,45 @@
+/// <reference types="cypress" />
+/// <reference path="../support/index.d.ts" />
+beforeEach('open Applicaton',()=>{
+    cy.LoginToApp()
+})
+
+
+it('Check Navigation Links', () => {
+    const navLinks = [
+        { name: 'Home',     tag: 'a'      },
+        { name: 'Products', tag: 'a'      },
+        { name: 'Orders',   tag: 'button' },
+        { name: 'About',    tag: 'a'      },
+    ]
+
+    navLinks.forEach(link => {
+        cy.visit('/')                          
+        cy.contains(link.tag, link.name)
+        
+          .click()
+    })
+})
+it('Products categories', () => {
+    cy.contains('Product').click()
+    cy.contains('span','Kéwi wallets').click()
+    
+});
+it('Wallets Categories', () => {
+    cy.contains('Product').click()
+    cy.contains('span','Kéwi wallets').click()
+    
+    cy.get('button[role="combobox"]').first().click()
+    cy.get('[role="option"]').contains('Medium').click()
+    cy.get('button[role="combobox"]').first()
+      .find('span')
+      .should('contain.text', 'Medium')
+});
+it.only('Delete Items', () => {
+
+   
+cy.get('button').find('svg.lucide-shopping-cart').first().parent('button').click()
+
+
+    
+});
