5162940eee488190af0dc7356546a299f9e078ee
RobaSamoudi <samoudiruba@gmail.com>
2026-05-06 19:42:24 +0300

    add

---
+++ cypress/e2e/AddItems.cy.js
@@ -0,0 +1,31 @@
+/// <reference types="cypress" />
+/// <reference path="../support/index.d.ts" />
+
+beforeEach('open Application', () => {
+    cy.LoginToApp()
+})
+it('Multi Product Order', () => {
+    cy.contains('Product').should('be.visible').click()
+
+    cy.contains('Kéwi bags').should('be.visible').click()
+    cy.get('[placeholder="Search in this category..."]')
+      .should('be.visible')
+      .click()
+      .type('Lady Dior')
+      cy.contains('h3', 'Lady Dior').first()
+      .should('be.visible').click()
+      cy.contains('button', 'Add to Cart')
+      .should('be.visible')
+      .and('be.enabled')
+      .click()
+cy.contains('button', 'Back to Products').click()
+ cy.contains('span','Kéwi wallets').click()
+  cy.contains('h3', 'LV wallet 12').click()
+  cy.contains('button', 'Add to Cart')
+      .should('be.visible')
+      .and('be.enabled')
+      .click()
+cy.AddToCart()
+    
+    
+});
