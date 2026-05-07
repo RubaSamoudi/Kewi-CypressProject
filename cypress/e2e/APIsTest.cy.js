e5804cd657a16ccc20875da0aa05eef57bd8caeb
RobaSamoudi <samoudiruba@gmail.com>
2026-05-07 12:53:34 +0300

    new

--- cypress/e2e/APIsTest.cy.js
+++ cypress/e2e/APIsTest.cy.js
@@ -1,10 +1,12 @@
 /// <reference types="cypress" />
 /// <reference path="../support/index.d.ts" />
 
-beforeEach('open Applicaton',()=>{
-    cy.LoginToApp()
-})
+
 it('ADDitem', () => {
+     cy.visit('/')
+     cy.contains('a','Products').click()
     cy.intercept('GET', '**/products', { fixture: 'products.json' })
     cy.intercept('GET', '**/categories*', { fixture: 'categories.json' })
+    
 });
+
